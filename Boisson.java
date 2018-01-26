44ac022e4ecd26c55a67bb0f13ba993cd0250952
TomGall <thomasgall@free.fr>
2018-01-26 14:30:02 +0100

    add boisson, ingredient, machine

---
+++ Boisson.java
@@ -0,0 +1,16 @@
+class Boisson {
+
+	public String nom;
+	public Ingredient() ingredients;
+	public int prix;
+
+	public Boisson(Ingredient() ingredients, String nom, int prix) {
+
+		this.ingredients = ingredients;
+		this.nom = nom;
+		this.prix = prix;
+	}
+
+
+	
+}
---
+++ Machine.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Machine {
+
+	public Boisson[3] boissons;
+	public int stockCafe;
+	public int stockLait;
+	public int stockChocolat;
+	public int stockSucre;
+
+
+	
+}
